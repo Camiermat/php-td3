e13ed393672b00fe181950202aa01d67b7dd19f1
tim <timdaurelle@gmail.com>
2019-10-22 10:20:13 +0200

    add query row

--- models/PhotoDAO.php
+++ models/PhotoDAO.php
@@ -4,7 +4,7 @@ require_once(PATH_MODEL,'DAO.php');
 class PhotoDAO extends DAO {
 	
 	public function __getPhoto($i){
-		$res = $this->queryAll('SELECT * FROM Photo where photoId = ?',array($i));
+		$res = $this->queryRow('SELECT * FROM Photo where photoId = ?',array($i));
 		if ($res){
 			require_once(PATH_UTILITIES,'Photo.php');
 			return new Photo($res['photoId'],$res['nomFich'],$res['description'],$res['catId']);
